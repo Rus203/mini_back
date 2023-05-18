ce985a30324c80d5080f99511a1faae4eb052109
Ruslan <c8man@gmail.com>
2023-05-19 00:21:33 +0300

    fix: remove controller

--- src/server/server.module.ts
+++ src/server/server.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ServerController } from './server.controller';
 import { ServerGateway } from './server.gateway';
 
 @Module({
-  controllers: [ServerController],
   providers: [ServerGateway]
 })
 export class ServerModule {}
