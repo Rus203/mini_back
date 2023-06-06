a352c0c9d5c026bf7fdc313444eea5add08ee823
Ruslan <c8man@gmail.com>
2023-06-06 17:10:30 +0200

    fix: minor fix

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,4 @@ export * from './cleanDir';
 export * from './readStreamToString';
 export * from './handleServiceErrors';
 export * from './export-ports-from-docker';
-export * from './check-port-availability ';
+export * from './check-port-availability';
