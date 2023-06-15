55b7589bfd2f787d862e9fcf763bad353e2bdc44
Ruslan <c8man@gmail.com>
2023-06-16 01:46:47 +0300

    remove garbage

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
@@ -10,7 +8,6 @@ import { MailersModule } from './mailers';
 import { DatabaseModule } from './database/database.module';
 import { DockerModule } from './docker/docker.module';
 import { PortModule } from './port/port.module';
-import { SocketDeployGateway } from './socket-deploy/socket-deploy.gateway';
 import { SocketDeployModule } from './socket-deploy/socket-deploy.module';
 import { SocketProgressModule } from './socket-progress/socket-progress.module';
 import { FileEncryptorModule } from './file-encryptor/file-encryptor.module';
@@ -30,7 +27,7 @@ import { FileEncryptorModule } from './file-encryptor/file-encryptor.module';
     SocketProgressModule,
     FileEncryptorModule
   ],
-  controllers: [AppController],
-  providers: [AppService, SocketDeployGateway]
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  isWorking() {
-    return { status: true };
-  }
-}
